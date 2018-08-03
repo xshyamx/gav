9527f49c808b8d3b8f2f5514ff86c0f2ce62a4a0
shyam <xshyamx@gmail.com>
2018-08-04 05:08:15 +0530

    writing debug.json instead of printing to console for summary of results

--- main.go
+++ main.go
@@ -1,20 +1,22 @@
 package main
 
 import (
-  "crypto/sha1"
-  "flag"
-  "fmt"
-  "html/template"
-  "io"
-  "os"
-  "path/filepath"
-  "strings"
+	"crypto/sha1"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"html/template"
+	"io"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strings"
 )
 
 var (
-  debug        bool
-  outFile      string
-  dependencies chan Dependency
+	debug        bool
+	outFile      string
+	dependencies chan Dependency
 
 //  wg           sync.WaitGroup
 )
@@ -43,93 +45,125 @@ const pomTemplate = `<project xmlns="http://maven.apache.org/POM/4.0.0"
 </project>`
 
 type Dependency struct {
-  GroupId    string `json:"groupId"`
-  ArtifactId string `json:"artifactId"`
-  Version    string `json:"version"`
+	GroupId    string `json:"groupId"`
+	ArtifactId string `json:"artifactId"`
+	Version    string `json:"version"`
+}
+
+type Result struct {
+	Path       string      `json:"path"`
+	Filename   string      `json:"filename"`
+	Hash       string      `json:"sha1"`
+	Dependency *Dependency `json:"dependency,omitempty"`
 }
 
 type RepoFunc func(string) (Dependency, error)
 
 var repoFuncs = []RepoFunc{
-  central,
-  jboss,
-  spring,
-  jfrog,
+	central,
+	jboss,
+	spring,
+	jfrog,
 }
 
 // print debug messages to console
 func debugf(format string, args ...interface{}) {
-  if debug {
-    fmt.Println("[DEBUG] " + fmt.Sprintf(format, args...))
-  }
+	if debug {
+		fmt.Println("[DEBUG] " + fmt.Sprintf(format, args...))
+	}
 }
 
 func getHash(path string) (string, error) {
-  f, err := os.Open(path)
-  if err != nil {
-    return "", err
-  }
-  defer f.Close()
+	f, err := os.Open(path)
+	if err != nil {
+		return "", err
+	}
+	defer f.Close()
 
-  h := sha1.New()
-  if _, err := io.Copy(h, f); err != nil {
-    return "", err
-  }
-  return fmt.Sprintf("%x", h.Sum(nil)), nil
+	h := sha1.New()
+	if _, err := io.Copy(h, f); err != nil {
+		return "", err
+	}
+	return fmt.Sprintf("%x", h.Sum(nil)), nil
 }
 
 func main() {
-  flag.BoolVar(&debug, "d", false, "Print debug messages")
-  flag.StringVar(&outFile, "o", "pom.xml", "Output file name")
-  flag.Parse()
-  nDirs := flag.NArg()
-  if nDirs == 0 {
-    flag.PrintDefaults()
-  }
-  debugf("debug: %t, outFile: %s, dirs: %d", debug, outFile, nDirs)
-  var deps []Dependency
-  dirs := flag.Args()
-  var found, total = 0, 0
-  for _, dir := range dirs {
-    debugf("Scanning %s", dir)
-    filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
-      if err != nil {
-        return err
-      }
-      if !info.IsDir() {
-        // file
-        if strings.HasSuffix(path, ".jar") {
-          hash, err := getHash(path)
-          if err != nil {
-            return err
-          }
-          debugf("%s <- %s", hash, filepath.Base(path))
-          total++
-          for _, repoFunc := range repoFuncs {
-            if dep, err := repoFunc(hash); err != nil {
-              // failed one
-              debugf("Failed to find dependency from %v", err)
-            } else {
-              debugf("%v, %v", dep, err)
-              deps = append(deps, dep)
-              found++
-              break
-            }
-          }
-        }
-      }
-      return nil
-    })
-  }
-  pomTmpl := template.Must(template.New("pom").Parse(pomTemplate))
-  debugf("dependencies = %v", deps)
-  out, err := os.Create(outFile)
-  if err != nil {
-    debugf("Failed to open file %s", outFile)
-    panic(err)
-  }
-  err = pomTmpl.Execute(out, deps)
-  if err == nil {
-    fmt.Printf("%d dependencies out of %d jars\n", found, total)
-  }
+	flag.BoolVar(&debug, "d", false, "Print debug messages")
+	flag.StringVar(&outFile, "o", "pom.xml", "Output file name")
+	flag.Parse()
+	nDirs := flag.NArg()
+	if nDirs == 0 {
+		flag.PrintDefaults()
+	}
+	debugf("debug: %t, outFile: %s, dirs: %d", debug, outFile, nDirs)
+	var deps []Dependency
+	var results []Result
+	dirs := flag.Args()
+	var found, total = 0, 0
+	for _, dir := range dirs {
+		debugf("Scanning %s", dir)
+		filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
+			if err != nil {
+				return err
+			}
+			if !info.IsDir() {
+				// file
+				if strings.HasSuffix(path, ".jar") {
+					hash, err := getHash(path)
+					if err != nil {
+						return err
+					}
+					relPath, _ := filepath.Rel(dir, path)
+					result := Result{
+						Path:     relPath,
+						Filename: filepath.Base(path),
+						Hash:     hash,
+					}
+					debugf("%s <- %s", result.Hash, result.Filename)
+					total++
+					for _, repoFunc := range repoFuncs {
+						if dep, err := repoFunc(result.Hash); err != nil {
+							// failed one
+							result.Dependency = nil
+							debugf("Failed to find dependency from %v", err)
+						} else {
+							debugf("[%s] %s -> %+v, %v", hash, filepath.Base(path), dep, err)
+							result.Dependency = &dep
+							deps = append(deps, dep)
+							found++
+							break
+						}
+					}
+					results = append(results, result)
+				}
+			}
+			return nil
+		})
+	}
+	pomTmpl := template.Must(template.New("pom").Parse(pomTemplate))
+	out, err := os.Create(outFile)
+	if err != nil {
+		debugf("Failed to open file %s", outFile)
+		panic(err)
+	}
+	err = pomTmpl.Execute(out, deps)
+	if err == nil {
+		fmt.Printf("%d dependencies out of %d jars\n", found, total)
+	}
+	if debug {
+		buf, err := json.MarshalIndent(struct {
+			BaseDirs []string `json:"basedirs"`
+			Results  []Result `json:"results"`
+		}{
+			dirs,
+			results,
+		}, "", "  ")
+		if err != nil {
+			debugf("Failed to marshal debug results json")
+			// don't panic
+		}
+		if err = ioutil.WriteFile("debug.json", buf, 0655); err != nil {
+			debugf("Failed to write debug.json")
+		}
+	}
 }
